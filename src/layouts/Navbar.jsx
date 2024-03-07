962b9fb387d4a3bbb2ea43de4e4e46783ebfb010
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-03-06 16:39:52 -0800

    responsive navbar revision pt1

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,23 +1,43 @@
 import { Close, DragHandle } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function Navbar() {
-
     const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const toggleMenu = () => setIsMenuVisible(!isMenuVisible);
 
+    // Update the windowWidth state whenever the window resizes
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+
+        // Check initially (when component mounts)
+        handleResize();
+
+        // Cleanup the event listener when the component unmounts
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    // Automatically hide menu and show drag handle icon when window width is less than 900px
+    useEffect(() => {
+        if (windowWidth < 900) {
+            setIsMenuVisible(false); // This will hide the menu
+        } else {
+            setIsMenuVisible(true); // This will show the menu
+        }
+    }, [windowWidth]); // This effect depends on windowWidth
+
     return (
         <div className='navbar'>
-
-            {/* Replace "Welldone" with an <img> tag */}
             <a className='logo' href="#home">
                 <img className='logoImg' src="https://firebasestorage.googleapis.com/v0/b/ally-health-obgyn.appspot.com/o/design-portfolio-logo.png?alt=media&token=50d72de4-76d4-4dc5-8d25-946fb0d5ffb4" alt="Welldone" style={{ height: '50px' }} />
             </a>
 
             <div className='navigation'>
-
                 <IconButton onClick={toggleMenu} className='menuBtn'>
                     {!isMenuVisible &&
                         <DragHandle className='menuIcon' />
@@ -32,11 +52,8 @@ export default function Navbar() {
                     <li><a className='menuItem' href="#home">Home</a></li>
                     <li><a className='menuItem' href="#home">Portfolio</a></li>
                     <li><a className='menuItem' href="#home">Services</a></li>
-                    <li><a className='menuItem' href="#home">About</a></li>
-                    <li><a className='menuItem' href="#home">Pricing</a></li>
-                    <li><a className='menuItem' href="#home">Contact</a></li>
                 </ul>
             </div>
-        </div >
-    )
+        </div>
+    );
 }
